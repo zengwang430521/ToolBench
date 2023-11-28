dc607efdbe7a0d9d23de3f31bbc1300776adf08b
zengwang <zengwang430521@gmail.com>
2023-11-28 15:13:06 +0800

    dist

--- cmd_train_thought.sh
+++ cmd_train_thought.sh
@@ -5,6 +5,7 @@ ssh -N -f -L 6000:10.142.4.31:20 -p 20 zengwang@jump-bj.sensetime.com -o TCPKeep
 
 
 export PYTHONPATH=./
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
 
 srun -p pat_taurus --job-name=toolllama \
    --ntasks=1 --gres=gpu:8 --ntasks-per-node=1 --cpus-per-task=40 --kill-on-bad-exit=1 \
@@ -30,7 +31,7 @@ torchrun --nproc_per_node=8 --master_port=20001 \
     --weight_decay 0. \
     --warmup_ratio 0.04 \
     --lr_scheduler_type "cosine" \
-    --logging_steps 1 \ 
+    --logging_steps 1 \
     --fsdp "full_shard auto_wrap" \
     --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
     --tf32 True \
@@ -51,22 +52,22 @@ torchrun --nproc_per_node=8 --master_port=20001 \
 export PYTHONPATH=./
 
 
-srun -p pat_taurus --quotatype=spot --job-name=toolllama \
+srun -p pat_taurus  --job-name=toolllama \
    --ntasks=1 --gres=gpu:8 --ntasks-per-node=1 --cpus-per-task=40 --kill-on-bad-exit=1 \
    \
 torchrun --nproc_per_node=8 --master_port=20001 \
     toolbench/train/thought_train_long_seq_debug.py \
     --data_path  data/toolllama_G123_dfs_train_light.json \
     --eval_data_path  data/toolllama_G123_dfs_eval.json \
-    --output_dir work_dirs/thought/split \
+    --output_dir work_dirs/thought/split_wo_distillation \
     \
     --model_name_or_path /mnt/lustre/zengwang/data/llama/huggyllama/llama-7b  \
     --conv_template tool-llama-single-round \
     --bf16 True \
     --num_train_epochs 2 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
-    --gradient_accumulation_steps 2 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 16 \
     --evaluation_strategy "epoch" \
     --prediction_loss_only \
     --save_strategy "epoch" \
@@ -84,6 +85,12 @@ torchrun --nproc_per_node=8 --master_port=20001 \
     --gradient_checkpointing True \
     --lazy_preprocess True \
     --report_to none
+
+
+
+
+
+
 
 
 
